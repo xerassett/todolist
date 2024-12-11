7527eb63c952e947b72f4f84174e2bfad02d10fd
Alex Fioretzi--Ysabel <xerassett@gmail.com>
2024-12-11 22:58:53 +0100

    Code Fini

--- todolist/TODOLIST.php
+++ todolist/TODOLIST.php
@@ -1,10 +1,13 @@
+<?php
+    session_start(); 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="TODOLIST.css">
-    <title>to do list</title>
+    <title>To-Do List</title>
 </head>
 <body>
     <div id="corp">  
@@ -12,21 +15,39 @@
         <div id="searchbarre">
             <form method="POST">
                 <input id="add" type="text" name="task" placeholder="Add Todo...">
-                <button type="submit" onclick="addTask()">ADD</button>
+                <button type="submit" name="add">ADD</button>
             </form>
         </div>
         <?php 
-            if (isset($_POST["task"]) && !empty($_POST["task"])) {
-                $task = [htmlspecialchars($_POST["task"])];
-                echo "<div class=opt>";
-                foreach($task as $item) {
-                    echo "<option>" . $item . "</option>";
-                    echo "<button type=submit name=delete>X</button>";
-                }
-                echo "</div>";
+            if (!isset($_SESSION['tasks'])) {
+                $_SESSION['tasks'] = [];
             }
-
-        ?>      
+            if (isset($_POST['add']) && !empty(trim($_POST['task']))) {
+                $task = htmlspecialchars($_POST['task']);
+                $_SESSION['tasks'][] = $task;
+            }
+            if (isset($_POST['delete'])) {
+                $index = intval($_POST['delete']);
+                if (isset($_SESSION['tasks'][$index])) {
+                    unset($_SESSION['tasks'][$index]);
+                    $_SESSION['tasks'] = array_values($_SESSION['tasks']); 
+                }
+            }
+            if (!empty($_SESSION['tasks'])) {
+                echo '<ul>';
+                foreach ($_SESSION['tasks'] as $index => $item) {
+                    echo '<li>';
+                    echo htmlspecialchars($item);
+                    echo ' <form method="POST" style="display:inline;">';
+                    echo '<button type="submit" name="delete" value="' . $index . '">X</button>';
+                    echo '</form>';
+                    echo '</li>';
+                }
+                echo '</ul>';
+            } else {
+                echo '<p>Pas de tâches actuellement.</p>';
+            }
+        ?>    
     </div>
 </body>
 </html>
